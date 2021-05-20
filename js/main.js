3d9fa17eb498ce0b6906a3779fc96ea1f6f97497
Robbie0999 <73835234+Robbie0999@users.noreply.github.com>
2021-05-20 11:54:38 +0200

    Newest main.js
    Newest update main.js

--- js/main.js
+++ js/main.js
@@ -60,31 +60,36 @@ function scrollTop(){
 
 window.addEventListener('scroll', scrollTop)
 
-/* MIXITUP PORTFOLIO */
-const mixer = mixitup('.portfolio__container', {
-    selectors: {
-        target: '.portfolio__content'
-    },
-    animation: {
-        duration: 400
-    }
-});
-
-/* LINK ACTIEVE PORTFOLIO CLICK */
-const linkPortfolio = document.querySelectorAll('.portfolio__item')
-function activePortfolio(){
-    if(linkPortfolio){
-        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
-        this.classList.add('active-portfolio')
-    }
-}
-
-linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
-
 /* GSAP ANIMATIE */
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x: 60})
-gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y: 25})
-gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger: .2})
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger: .2})
-gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y: 25, ease:'expo.out', stagger: .2})
-gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2.3, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger: .2})
+
+/* AOS */
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+
+/* CURSOR */
+const cursor = document.querySelector('.cursor')
+
+document.addEventListener('mousemove', e =>{
+    cursor.setAttribute("style", "top: "+(e.pageY -10)+"px; left: "+(e.pageX -10)+"px;")
+})
+
+document.addEventListener('click', () =>{
+    cursor.classList.add("expand");
+    setTimeout(() =>{
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+/* IMG SLIDER */
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+
+    if(counter > 4){
+        counter = 1;
+    }
+}, 4000)
